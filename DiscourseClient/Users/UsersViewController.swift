cb9ae72c1f05728593d1e4fe46581ceeb176cea6
Javier Laguna <javier.laguna92@gmail.com>
2020-05-23 18:57:54 +0200

    configureNavigationBar on users section

--- DiscourseClient/Users/UsersViewController.swift
+++ DiscourseClient/Users/UsersViewController.swift
@@ -16,11 +16,11 @@ class UsersViewController: UIViewController {
         let sectionInset: CGFloat = 24
         let minimumInteritemSpacing: CGFloat = 34
         let minimumLineSpacing: CGFloat = 19
-
+        let cellHeight: CGFloat = 123
         
         let layout = UICollectionViewFlowLayout()
         let width: CGFloat = (UIScreen.main.bounds.width - sectionInset * 2 - minimumInteritemSpacing * (CGFloat(numberOfColumns) - 1)) / CGFloat(numberOfColumns)
-        layout.itemSize = CGSize(width: width, height: 123)
+        layout.itemSize = CGSize(width: width, height: cellHeight)
         layout.minimumInteritemSpacing = minimumInteritemSpacing
         layout.minimumLineSpacing = minimumLineSpacing
         layout.sectionInset = UIEdgeInsets(top: sectionInset, left: sectionInset, bottom: sectionInset, right: sectionInset)
@@ -37,6 +37,7 @@ class UsersViewController: UIViewController {
         return collectionView
     }()
     
+    lazy var searchBar = UISearchBar()
     let viewModel: UsersViewModel
     
     init(viewModel: UsersViewModel) {
@@ -62,12 +63,57 @@ class UsersViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureNavigationBar()
+        configureSearchBar()
         viewModel.viewWasLoaded()
+    }
+    
+    @objc func plusButtonTapped() {
+        // TODO implement
+    }
+    
+    @objc private func enableSearchBar() {
+        navigationItem.leftBarButtonItems = nil
+        navigationItem.rightBarButtonItems = nil
+        searchBar.text = ""
+        navigationItem.titleView = searchBar
+        searchBar.becomeFirstResponder()
+    }
+    
+    private func configureSearchBar() {
+        searchBar.delegate = self
+        searchBar.showsCancelButton = true
+        searchBar.tintColor = .orangeKCPumpkin
+        
+        let textFieldInsideUISearchBar = searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideUISearchBar?.textColor = .blackKC
+        searchBar.setImage(UIImage(), for: .search, state: .normal)
+    }
+    
+    private func configureNavigationBar() {
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.titleView = nil
+        
+        let addIcon = UIImage(named: "icoAdd")?.withRenderingMode(.alwaysTemplate)
+        let leftBarButtonItem: UIBarButtonItem = UIBarButtonItem(image: addIcon, style: .plain, target: self, action: #selector(plusButtonTapped))
+        leftBarButtonItem.tintColor = .orangeKCPumpkin
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+        
+        let serachIcon = UIImage(named: "icoSearch")?.withRenderingMode(.alwaysTemplate)
+        let rightBarButtonItem: UIBarButtonItem = UIBarButtonItem(image: serachIcon, style: .plain, target: self, action: #selector(enableSearchBar))
+        rightBarButtonItem.tintColor = .orangeKCPumpkin
+        navigationItem.rightBarButtonItem = rightBarButtonItem
     }
     
     fileprivate func showErrorFetchingUsersAlert() {
         let alertMessage: String = NSLocalizedString("Error fetching users\nPlease try again later", comment: "")
         showAlert(alertMessage)
+    }
+    
+    private func searchUsers(by text: String?) {
+        // TODO implement topic search
     }
 }
 
@@ -102,5 +148,28 @@ extension UsersViewController: UsersViewDelegate {
     
     func errorFetchingUsers() {
         showErrorFetchingUsersAlert()
+    }
+}
+
+// MARK: UISearchBarDelegate
+extension UsersViewController: UISearchBarDelegate {
+    
+    func cancelSearchFilter() {
+        searchUsers(by: nil)
+        searchBar.endEditing(true)
+        configureNavigationBar()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchUsers(by: searchText)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        cancelSearchFilter()
+        searchBar.text = ""
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
     }
 }
