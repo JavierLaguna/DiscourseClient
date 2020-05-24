30c04a8b98b0abb96a0f13f77d6f82cbd08851df
Javier Laguna <javier.laguna92@gmail.com>
2020-05-24 16:40:53 +0200

    Add labels

--- DiscourseClient/Extensions/UIFont+Extensions.swift
+++ DiscourseClient/Extensions/UIFont+Extensions.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 extension UIFont {
+    static let logo = UIFont.systemFont(ofSize: 89, weight: .bold)
     static let bigTitle = UIFont.systemFont(ofSize: 22, weight: .bold)
     static let title = UIFont.systemFont(ofSize: 17, weight: .semibold)
     static let paragraph = UIFont.systemFont(ofSize: 17, weight: .regular)
--- DiscourseClient/Scenes/Splash/SplashViewController.swift
+++ DiscourseClient/Scenes/Splash/SplashViewController.swift
@@ -9,26 +9,27 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureBackground()
+        configureLabels()
     }
     
     private func configureBackground() {
         view.backgroundColor = .blackKC
         
         let customShapeView = createBackgroundShapeView(frame: view.frame)
-              view.addSubview(customShapeView)
+        view.addSubview(customShapeView)
     }
     
     private func createBackgroundShapeView(frame: CGRect) -> UIView {
         let customShapeView = UIView(frame: frame)
         let customShapeLayer = CAShapeLayer()
-
+        
         customShapeView.layer.addSublayer(customShapeLayer)
-
+        
         let bezierPath = UIBezierPath()
         bezierPath.move(to: .zero)
         bezierPath.addLine(to: CGPoint(x: frame.width, y: 0))
@@ -36,9 +37,48 @@ class SplashViewController: UIViewController {
         bezierPath.close()
         
         customShapeLayer.path = bezierPath.cgPath
-
+        
         customShapeLayer.fillColor = UIColor.grayKC.cgColor
         
         return customShapeView
+    }
+    
+    private func configureLabels() {
+        let ehLabel = UILabel()
+        ehLabel.translatesAutoresizingMaskIntoConstraints = false
+        ehLabel.text = "eh"
+        ehLabel.font = .logo
+        ehLabel.textColor = .white
+        
+        view.addSubview(ehLabel)
+        NSLayoutConstraint.activate([
+            ehLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            ehLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 32)
+        ])
+        
+        let ohLabel = UILabel()
+        ohLabel.translatesAutoresizingMaskIntoConstraints = false
+        ohLabel.text = "oh"
+        ohLabel.font = .logo
+        ohLabel.textColor = .white
+        
+        view.addSubview(ohLabel)
+        NSLayoutConstraint.activate([
+            ohLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            ohLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -32)
+        ])
+        
+        let separatorView = UIView(frame: .zero)
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.backgroundColor = .white
+        
+        view.addSubview(separatorView)
+        NSLayoutConstraint.activate([
+            separatorView.topAnchor.constraint(equalTo: ehLabel.bottomAnchor),
+            separatorView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            separatorView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            separatorView.heightAnchor.constraint(equalToConstant: 8)
+
+        ])
     }
 }
